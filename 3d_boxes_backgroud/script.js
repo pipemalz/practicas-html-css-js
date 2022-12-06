6de3a08322806b0169f083df2d3acb38845e6246
pipemalz <sinister012@gmail.com>
2022-12-05 21:06:17 -0500

    Recreando la misma funcionalidad con javascript

--- 3d_boxes_backgroud/script.js
+++ 3d_boxes_backgroud/script.js
@@ -5,3 +5,12 @@ button.addEventListener('click', () => {
     boxesContainer.classList.toggle('big');
     console.log(boxesContainer)
 });
+
+for(let i = 0; i < 4; i++){
+    for(let j =0; j < 4; j++){
+        const box = document.createElement('div');
+        box.classList.add('box');
+        box.style.backgroundPosition = `${-j*125}px ${-i*125}px`
+        boxesContainer.appendChild(box);
+    }
+}
