eb24360755afe458cf3d6910b69ada246be8b3b2
pipemalz <sinister012@gmail.com>
2022-12-05 20:48:32 -0500

    recreando el resultado manualmente con html y css

--- 3d_boxes_backgroud/script.js
+++ 3d_boxes_backgroud/script.js
@@ -1,13 +1,7 @@
-// const boxesContainer = document.querySelector("#boxes");
-// const button = document.querySelector("#button");
+const boxesContainer = document.querySelector("#boxes");
+const button = document.querySelector("#button");
 
-// button.addEventListener('click', () => {
-//     boxesContainer.classList.toggle('big');
-//     console.log(boxesContainer)
-// });
-
-
-
-
-// console.log(boxesContainer);
-// console.log(button.innerHTML);
+button.addEventListener('click', () => {
+    boxesContainer.classList.toggle('big');
+    console.log(boxesContainer)
+});
